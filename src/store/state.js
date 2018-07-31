e2e1bfa17497d07906117bc4ccb4319d30ed75e3
vinnyfs89 <viniciusfesil@gmail.com>
2018-07-31 01:30:55 -0300

    :zap: Adicionando uma store e state iniciais

---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+export default {
+    user: {
+        name: 'Vinicius Feitosa da Silva',
+        email: 'viniciusfesil@gmail.com',
+        level: 'admin-da-porra-toda'
+    },
+    token: 'roiaoiejoirjoiajr3'
+}
