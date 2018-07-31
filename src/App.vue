f6ad024d9988ae292702126b57694a58e2a85bff
vinnyfs89 <viniciusfesil@gmail.com>
2018-07-31 01:48:48 -0300

    :zap: Adicionando primeiro componente

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script>
+    import CcUsers from './components/users/main.vue'
+
     export default {
+        components: {
+            CcUsers
+        },
         computed: {
             user() {
-                const { name, email } = this.$store.state.user
+                const {name, email} = this.$store.state.user
 
                 return `O Usuário logado é ${name} e possui o e-mail ${email}`
             }
@@ -13,5 +18,7 @@
     <div>
         <h1>Welcome Vue2</h1>
         <h4>{{ user }}</h4>
+        <hr />
+        <cc-users></cc-users>
     </div>
 </template>
---
+++ src/components/users/main.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        computed: {
+            email() {
+                return ''
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        Email : {{ email }}
+    </div>
+</template>
+
+<!--<style scoped>-->
+
+<!--</style>-->
