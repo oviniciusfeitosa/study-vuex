8ba9a61a94abe6daf28de07f6ffdb455bf881513
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-01 01:40:25 -0300

    :zap: Adição de módulo Categories. https://youtu.be/BT1kKaZwPRs?t=1930

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
 import mutations from './mutations'
-
+import categories from "./modules/categories/main"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state,
-    mutations
+    mutations,
+    modules: {
+        categories
+    }
 })
